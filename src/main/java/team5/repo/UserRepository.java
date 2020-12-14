426b7b0d2199e014776138222a1870ed28d2208f
cqtan-chloe <cqtan-chloe@gmail.com>
2020-12-14 13:39:19 +0800

    login first commit

--- src/main/java/team5/repo/UserRepository.java
+++ src/main/java/team5/repo/UserRepository.java
@@ -1,10 +1,14 @@
 package team5.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import team5.model.User;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	public User findUserByUserName(String un);
+	
+	@Query("Select u from User u where u.userName LIKE :un")
+	public User findUserByUserName(@Param("un") String un);
 }
