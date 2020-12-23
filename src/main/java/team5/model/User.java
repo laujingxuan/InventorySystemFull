a89b278445e227214c158e70d18cc3b11d996f90
jingxuan94 <E0639666@u.nus.edu>
2020-12-23 11:56:39 +0800

    Changes on the user entity

--- src/main/java/team5/model/User.java
+++ src/main/java/team5/model/User.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -19,9 +19,9 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 	@Column(unique=true)
-	@NotNull
+    @Size(min=2,max=30)
     private String userName;
-	@NotNull
+	@NotEmpty
 	@Size(min=2,max=30)
 	private String password;
 	@NotNull
